7dc8b02c28fabc89484b13db441933aa698ac8c8
Drew Prescott <code@doprogress.com>
2014-10-19 21:23:29 -0500

    Transitions, fades, animations
    More videos watched

--- Level002/js/app.js
+++ Level002/js/app.js
@@ -11,22 +11,25 @@
   hi.text(dataPeople[_.random(0,3)].name).css("color", headerColor); // Chane header based upon random number
   $('p #toBold').css('font-weight', "bold"); // testing spans / and zeroing in on elements
 
+  hi.hide('slow').show('slow');
+
   //function for story area, to generate per data.
   function assignStory (num) { // take number from the while statement
     var x = $('#content').children().eq(num); // num is passed as the location of the current childe node
     return x; //return $('#content').children().eq(#current statue#) each itteration
     }
 
-  //itterate over length of content to populate story area.
+var panel = $('#redArea');
+panel.fadeOut(3000).delay(1000).fadeIn(3000);
 
+  //itterate over length of content to populate story area
     var i = 0, divLen = $('#content').children().length, dataLen = dataContent.length;
     while (i <= dataLen){ // for the length of the story content area, run
     assignStory(i).text(dataContent[i].story);
       if(divLen >= i){
         i++;
-      } else {
-        break;
-      }
-    }
+      }}
+
+
 
 })();
