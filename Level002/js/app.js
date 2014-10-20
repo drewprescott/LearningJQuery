09f2f2acd00d17e18236e92a93f2367dac4167e6
Drew Prescott <code@doprogress.com>
2014-10-20 15:21:35 -0500

    More additions
    Starting on this video next:
    https://www.safaribooksonline.com/library/view/learning-jquery-programming/9781926873725/part32.html

--- Level002/js/app.js
+++ Level002/js/app.js
@@ -1,34 +1,52 @@
 //app.js
 (function() {
     //LoDash
-  var colorChoice = prompt("What color should the header be?");
-  var headerColor = _.find(dataColor, function(dtClr) {
-    return dtClr === colorChoice;
-  });
+    var colorChoice = prompt("What color should the header be?");
+    var headerColor = _.find(dataColor, function(dtClr) {
+        return dtClr === colorChoice;
+    });
 
-  //Jquery
-  var hi = $('#hi');     //Dummy header query
-  hi.text(dataPeople[_.random(0,3)].name).css("color", headerColor); // Chane header based upon random number
-  $('p #toBold').css('font-weight', "bold"); // testing spans / and zeroing in on elements
+    //Jquery
+    var hi = $('#hi'); //Dummy header query
+    hi.text(dataPeople[_.random(0, 3)].name).css("color", headerColor); // Chane header based upon random number
+    $('p #toBold').css('font-weight', "bold"); // testing spans / and zeroing in on elements
 
-  hi.hide('slow').show('slow');
+    hi.hide('slow').show('slow');
 
-  //function for story area, to generate per data.
-  function assignStory (num) { // take number from the while statement
-    var x = $('#content').children().eq(num); // num is passed as the location of the current childe node
-    return x; //return $('#content').children().eq(#current statue#) each itteration
+    //function for story area, to generate per data.
+    function assignStory(num) { // take number from the while statement
+        var x = $('#content').children().eq(num); // num is passed as the location of the current childe node
+        return x; //return $('#content').children().eq(#current statue#) each itteration
     }
 
-  var cent = $('#callout');
-  cent.addClass('panel').fadeIn(1000);
+    $('#skills option').eq(2).prop('selected', true); //toggel the checkbox
 
-  //itterate over length of content to populate story area
-    var i = 0, divLen = $('#content').children().length, dataLen = dataContent.length;
-    while (i <= dataLen){ // for the length of the story content area, run
-    assignStory(i).text(randData[i].first_name).addClass('panel');
+    var cent = $('#callout');
+    cent.addClass('panel').fadeIn(3000);
 
-        i++;
-
+    $('li').text(
+      function( i, text ){
+        return ( i + 1 ) + ". " + text;
       }
+    );
+
+    $('#bio').html('<p>'  + dataContent[0].story + '<p>').addClass('panel').fadeOut(4000).show('fast');
+
+    var e = $('#cntr');
+    console.log( e.data('name') );
+    console.log( e.data('type') );
+    e.data("counter", 1);
+    console.log( e.data('counter') );
+
+
+    //itterate over length of content to populate story area
+    var i = 0,
+        divLen = $('#content').children().length,
+        dataLen = dataContent.length;
+    while (i <= dataLen) { // for the length of the story content area, run
+        assignStory(i).text(randData[i].first_name).addClass('panel');
+        i++;
+    }
+
 
 })();
